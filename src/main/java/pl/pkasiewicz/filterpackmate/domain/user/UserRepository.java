2e3f2db48252bcc1bbb3ded136a841391a36dbd4
piotrkasiewicz <ptrkasiewicz@gmail.com>
2025-04-19 16:44:10 +0200

    removed unused import

--- src/main/java/pl/pkasiewicz/filterpackmate/domain/user/UserRepository.java
+++ src/main/java/pl/pkasiewicz/filterpackmate/domain/user/UserRepository.java
@@ -3,7 +3,6 @@ package pl.pkasiewicz.filterpackmate.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Repository
