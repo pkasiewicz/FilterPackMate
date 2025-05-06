a05fa94e15cc8d95fd2530e9026027ca1df80db6
piotrkasiewicz <ptrkasiewicz@gmail.com>
2025-05-06 20:53:09 +0200

    refactoring divider module - added config for facade and new method in mapper

--- src/main/java/pl/pkasiewicz/filterpackmate/domain/divider/DividerMapper.java
+++ src/main/java/pl/pkasiewicz/filterpackmate/domain/divider/DividerMapper.java
@@ -1,21 +1,42 @@
 package pl.pkasiewicz.filterpackmate.domain.divider;
 
+import pl.pkasiewicz.filterpackmate.domain.divider.dto.DividerDto;
 import pl.pkasiewicz.filterpackmate.domain.divider.dto.DividerRequestDto;
 import pl.pkasiewicz.filterpackmate.domain.divider.dto.DividerResponseDto;
+import pl.pkasiewicz.filterpackmate.domain.product.ProductMapper;
+import pl.pkasiewicz.filterpackmate.domain.product.dto.ProductSummaryDto;
 
-class DividerMapper {
+import java.util.List;
+import java.util.Optional;
+
+public class DividerMapper {
 
     static Divider mapToEntity(DividerRequestDto dto) {
         return Divider.builder()
                 .name(dto.name())
+                .quantityPerPallet(dto.quantityPerPallet())
                 .build();
     }
 
-    static DividerResponseDto mapToDto(Divider entity) {
+    static DividerResponseDto mapToDividerResponseDto(Divider entity) {
         return DividerResponseDto.builder()
-                .id(entity.id)
-                .name(entity.name)
-                .products(entity.products)
+                .id(entity.getId())
+                .name(entity.getName())
+                .products(
+                        Optional.ofNullable(entity.getProducts())
+                                .orElseGet(List::of)
+                                .stream()
+                                .map(ProductMapper::mapToProductSummaryDto)
+                                .toList()
+                )
+                .build();
+    }
+
+    public static DividerDto mapToDividerDto(Divider entity) {
+        return DividerDto.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .quantityPerPallet(entity.getQuantityPerPallet())
                 .build();
     }
 }
