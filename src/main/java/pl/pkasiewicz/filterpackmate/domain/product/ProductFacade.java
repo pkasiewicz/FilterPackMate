a83205c12495fcdba8251dbb2b841d77010ba72f
piotrkasiewicz <ptrkasiewicz@gmail.com>
2025-05-06 21:17:42 +0200

    refactoring product module - updated facade, mapper, added dto and config class for facade

--- src/main/java/pl/pkasiewicz/filterpackmate/domain/product/ProductFacade.java
+++ src/main/java/pl/pkasiewicz/filterpackmate/domain/product/ProductFacade.java
@@ -1,9 +1,17 @@
 package pl.pkasiewicz.filterpackmate.domain.product;
 
 import lombok.AllArgsConstructor;
+import pl.pkasiewicz.filterpackmate.domain.carton.Carton;
+import pl.pkasiewicz.filterpackmate.domain.carton.CartonFacade;
+import pl.pkasiewicz.filterpackmate.domain.divider.Divider;
+import pl.pkasiewicz.filterpackmate.domain.divider.DividerFacade;
 import pl.pkasiewicz.filterpackmate.domain.product.dto.ProductRequestDto;
 import pl.pkasiewicz.filterpackmate.domain.product.dto.ProductResponseDto;
 import pl.pkasiewicz.filterpackmate.domain.product.exceptions.ProductNotFoundException;
+import pl.pkasiewicz.filterpackmate.domain.side.Side;
+import pl.pkasiewicz.filterpackmate.domain.side.SideFacade;
+import pl.pkasiewicz.filterpackmate.domain.tray.Tray;
+import pl.pkasiewicz.filterpackmate.domain.tray.TrayFacade;
 
 import java.util.List;
 
@@ -11,10 +19,14 @@ import java.util.List;
 public class ProductFacade {
 
     private final ProductRepository productRepository;
+    private final CartonFacade cartonFacade;
+    private final TrayFacade trayFacade;
+    private final DividerFacade dividerFacade;
+    private final SideFacade sideFacade;
 
-    public ProductResponseDto saveProduct(ProductRequestDto product) {
-        Product savedProduct = productRepository.save(ProductMapper.mapToEntity(product));
-        return ProductMapper.mapToDto(savedProduct);
+    public ProductResponseDto saveProduct(ProductRequestDto dto) {
+        Product product = getProduct(dto);
+        return ProductMapper.mapToDto(productRepository.save(product));
     }
 
     public ProductResponseDto getProductById(Long id) {
@@ -27,6 +39,43 @@ public class ProductFacade {
         return productRepository.findAll()
                 .stream()
                 .map(ProductMapper::mapToDto)
+                .toList();
+    }
+
+    private Product getProduct(ProductRequestDto dto) {
+        Carton carton = getCarton(dto);
+        Tray tray = getTray(dto);
+        List<Divider> dividers = getDividers(dto);
+        List<Side> sides = getSides(dto);
+
+        return Product.builder()
+                .carton(carton)
+                .tray(tray)
+                .pallet(dto.pallet())
+                .dividers(dividers)
+                .sides(sides)
+                .build();
+    }
+
+    private Carton getCarton(ProductRequestDto dto) {
+        return cartonFacade.getCartonEntityById(dto.cartonId());
+    }
+
+    private Tray getTray(ProductRequestDto dto) {
+        return trayFacade.getTrayEntityById(dto.trayId());
+    }
+
+    private List<Divider> getDividers(ProductRequestDto dto) {
+        return dto.dividerIds()
+                .stream()
+                .map(dividerFacade::getDividerEntityById)
+                .toList();
+    }
+
+    private List<Side> getSides(ProductRequestDto dto) {
+        return dto.sideIds()
+                .stream()
+                .map(sideFacade::getSideEntityById)
                 .toList();
     }
 }
