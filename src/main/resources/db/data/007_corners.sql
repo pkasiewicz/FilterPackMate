fb9ce583162fda4e03f09c48ed4b2410c1ed1009
piotrkasiewicz <ptrkasiewicz@gmail.com>
2025-05-09 15:39:50 +0200

    create table and sql file with init data for corners

---
+++ src/main/resources/db/data/007_corners.sql
@@ -0,0 +1,6 @@
+INSERT INTO corner (name) VALUES ('CP1000');
+INSERT INTO corner (name) VALUES ('CP750');
+INSERT INTO corner (name) VALUES ('CP800');
+INSERT INTO corner (name) VALUES ('CP850');
+INSERT INTO corner (name) VALUES ('CP900');
+INSERT INTO corner (name) VALUES ('CP950');
