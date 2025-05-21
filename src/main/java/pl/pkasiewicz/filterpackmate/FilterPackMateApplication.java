5c80dd691aebc85eae47664ca78c8ba1d7fc8330
piotrkasiewicz <ptrkasiewicz@gmail.com>
2025-05-21 22:54:25 +0200

    add JWT-based authentication setup

--- src/main/java/pl/pkasiewicz/filterpackmate/FilterPackMateApplication.java
+++ src/main/java/pl/pkasiewicz/filterpackmate/FilterPackMateApplication.java
@@ -2,8 +2,11 @@ package pl.pkasiewicz.filterpackmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import pl.pkasiewicz.filterpackmate.infrastructure.security.jwt.JwtConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({JwtConfigurationProperties.class})
 public class FilterPackMateApplication {
 
     public static void main(String[] args) {
