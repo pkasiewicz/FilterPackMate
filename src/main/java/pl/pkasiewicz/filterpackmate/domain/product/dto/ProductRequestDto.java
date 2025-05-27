9ab1c82af6777d5c27cfd9504e43ad5703de70b4
piotrkasiewicz <ptrkasiewicz@gmail.com>
2025-05-27 16:02:12 +0200

    update types in entity and dtos in product module

--- src/main/java/pl/pkasiewicz/filterpackmate/domain/product/dto/ProductRequestDto.java
+++ src/main/java/pl/pkasiewicz/filterpackmate/domain/product/dto/ProductRequestDto.java
@@ -1,19 +1,18 @@
 package pl.pkasiewicz.filterpackmate.domain.product.dto;
 
 import lombok.Builder;
-import pl.pkasiewicz.filterpackmate.domain.product.Pallet;
 
 import java.util.List;
 
 @Builder
 public record ProductRequestDto(
         String name,
-        int filtersPerCarton,
-        int cartonsPerPallet,
-        int filtersPerPallet,
+        Integer filtersPerCarton,
+        Integer cartonsPerPallet,
+        Integer filtersPerPallet,
         Long cartonId,
         Long trayId,
-        Pallet pallet,
+        String palletType,
         List<Long> dividerIds,
         List<Long> sideIds,
         Long cornerId
--- src/main/java/pl/pkasiewicz/filterpackmate/domain/product/dto/ProductResponseDto.java
+++ src/main/java/pl/pkasiewicz/filterpackmate/domain/product/dto/ProductResponseDto.java
@@ -2,7 +2,6 @@ package pl.pkasiewicz.filterpackmate.domain.product.dto;
 
 import lombok.Builder;
 import pl.pkasiewicz.filterpackmate.domain.divider.dto.DividerDto;
-import pl.pkasiewicz.filterpackmate.domain.product.Pallet;
 import pl.pkasiewicz.filterpackmate.domain.side.dto.SideDto;
 
 import java.util.List;
@@ -15,10 +14,10 @@ public record ProductResponseDto (
         String cartonName,
         Long trayId,
         String trayName,
-        int filtersPerCarton,
-        int cartonsPerPallet,
-        int filtersPerPallet,
-        Pallet pallet,
+        Integer filtersPerCarton,
+        Integer cartonsPerPallet,
+        Integer filtersPerPallet,
+        String palletType,
         List<DividerDto> dividers,
         List<SideDto> sides,
         Long cornerId
