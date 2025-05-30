c4f214fcf73add5a15c1d1aa0cfd415f06f08a28
piotrkasiewicz <ptrkasiewicz@gmail.com>
2025-05-30 12:53:47 +0200

    change location of controllers and create a general error response instead of separate ones for each module

--- src/main/java/pl/pkasiewicz/filterpackmate/infrastructure/carton/controller/CartonController.java
+++ src/main/java/pl/pkasiewicz/filterpackmate/infrastructure/carton/controller/CartonController.java
@@ -1,4 +1,4 @@
-package pl.pkasiewicz.filterpackmate.infrastructure.carton;
+package pl.pkasiewicz.filterpackmate.infrastructure.carton.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
--- src/main/java/pl/pkasiewicz/filterpackmate/infrastructure/divider/controller/DividerController.java
+++ src/main/java/pl/pkasiewicz/filterpackmate/infrastructure/divider/controller/DividerController.java
@@ -1,4 +1,4 @@
-package pl.pkasiewicz.filterpackmate.infrastructure.divider;
+package pl.pkasiewicz.filterpackmate.infrastructure.divider.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
